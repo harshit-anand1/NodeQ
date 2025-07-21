71582cb7faf38aa9b9f1552f1bf6e039f9c32f0c
Harshit Anand <iamharshit101@gmail.com>
2025-07-21 22:44:11 +0530

    feat:  add Redis queue integration for job submission 2

--- client-api/index.js
+++ client-api/index.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {v4 : uuidv4} = require('uuid');
+//const {v4 : uuidv4} = require('uuid');
+const {enqueueJob} = require('../queue/index.js');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/submit', (req,res)=>{
+app.post('/submit', async (req,res)=>{
+   
+   try{
     const {type, payload}   = req.body;
     
     if(!type || !payload){
        return res.status(400).json({ error: 'Missing type or payload'});
     }
 
-    const jobId = uuidv4();
+    //const jobId = uuidv4();
+    const jobId = await enqueueJob({type,payload});
+
 
     //for now just log it later to redis
 
     console.log(`Job ${jobId} submitted:`, {type, payload});
 
     res.status(202).json({message: 'Job accepted', jobId});
-
+    } 
+    catch (err)
+    {
+    console.error('Job enqueue failed:', err);
+    res.status(500).json({error: 'Internal server error'});  
+}
 });
 
 const PORT = 3000;
