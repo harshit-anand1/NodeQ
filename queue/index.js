e7e4800be6c585513da4b405f7e7faba9f8b12a6
Harshit Anand <iamharshit101@gmail.com>
2025-07-21 22:36:52 +0530

    completed enqueue function

--- queue/index.js
+++ queue/index.js
@@ -2,3 +2,24 @@ const Redis = require('ioredis');
 const { v4 : uuidv4} = require('uuid');
 
 const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:379');
+
+async function enqueue(job) {
+    const jobId = uuidv4();
+    const jobData = {
+        id: jobId,
+        type: job.type,
+        payload: job.payload,
+        status: 'queued',
+        createdAt: Date.now()
+    };
+
+    //push to job queue
+    await redis.rpush('job', JSON.stringify(jobData));
+
+    //storing the metadata
+    await redis.hset(`job:${jobId}`, jobData);
+
+    return jobId;
+}
+
+module.exports = { enqueueJob, redis};
